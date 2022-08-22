05716832044b5a188b2624a342b10c90d397a3c0
Samuel Abreu <SAAH0001@ce.pucmm.edu.do>
2022-08-22 17:14:28 -0400

    reiniciando

--- prueba.py
+++ prueba.py
@@ -3,6 +3,7 @@ def run():
     print("hola como vas hace tiempo no se de ti")
     print("el sonido de la lataaa")
     nombre = input("¿Cual es tu nombre?: ")
+    print("Hola " + nombre)
 def main():
     run()
 
